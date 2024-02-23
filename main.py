a331cf2c36fd524756ed2e59e0317f1d71cf1764
Flamewiing <niklas-hohn@t-online.de>
2024-02-23 17:31:49 +0100

    update files

--- main.py
+++ main.py
@@ -4,16 +4,32 @@ from ortools.sat.python import cp_model
 def main():
     SearchForAllSolutionsSampleSat()
 
+class VarArraySolutionPrinter(cp_model.CpSolverSolutionCallback):
+    """Print intermediate solutions."""
+
+    def __init__(self, variables):
+        cp_model.CpSolverSolutionCallback.__init__(self)
+        self.__variables = variables
+        self.__solution_count = 0
+
+    def on_solution_callback(self):
+        self.__solution_count += 1
+        for v in self.__variables:
+            print(f"{v}={self.Value(v)}", end=" ")
+        print()
+
+    def solution_count(self):
+        return self.__solution_count
+
 def SearchForAllSolutionsSampleSat():
     """Showcases calling the solver to search for all solutions."""
     # Creates the model.
     model = cp_model.CpModel()
 
     # Creates the variables.
-    num_vals = 3
-    x = model.NewIntVar(0, num_vals - 1, "x")
-    y = model.NewIntVar(0, num_vals - 1, "y")
-    z = model.NewIntVar(0, num_vals - 1, "z")
+    lecturers = model.NewIntVar(1, 8, "Lecturer")
+    time_spot = model.NewIntVar(0, 10, "TimeSpot")
+    day = model.NewIntVar(0, 5, "Day")
 
     # Create the constraints.
     model.Add(x != y)
