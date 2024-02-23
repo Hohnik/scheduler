cfd59ec0d7a7cb92dc70dc5a23339ed52dc81933
Flamewiing <niklas-hohn@t-online.de>
2024-02-23 15:56:07 +0100

    example

--- main.py
+++ main.py
@@ -2,17 +2,32 @@ from ortools.sat.python import cp_model
 
 
 def main():
-    num_lecturers = 10
-    num_time_slots = 10
+    SearchForAllSolutionsSampleSat()
 
-    all_lecturers = range(num_lecturers)
-    all_slots = range(num_time_slots)
-
+def SearchForAllSolutionsSampleSat():
+    """Showcases calling the solver to search for all solutions."""
+    # Creates the model.
     model = cp_model.CpModel()
-    shifts = {}
-    for slot in all_slots:
 
+    # Creates the variables.
+    num_vals = 3
+    x = model.NewIntVar(0, num_vals - 1, "x")
+    y = model.NewIntVar(0, num_vals - 1, "y")
+    z = model.NewIntVar(0, num_vals - 1, "z")
 
+    # Create the constraints.
+    model.Add(x != y)
+
+    # Create a solver and solve.
+    solver = cp_model.CpSolver()
+    solution_printer = VarArraySolutionPrinter([x, y, z])
+    # Enumerate all solutions.
+    solver.parameters.enumerate_all_solutions = True
+    # Solve.
+    status = solver.Solve(model, solution_printer)
+
+    print(f"Status = {solver.StatusName(status)}")
+    print(f"Number of solutions found: {solution_printer.solution_count()}")
 
 if __name__ == "__main__":
     main()
