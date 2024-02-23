4face4fe9f17a041dc855a9e8b350417399c9f8c
Flamewiing <niklas-hohn@t-online.de>
2024-02-23 14:29:31 +0100

    Commit: 2024/02/23 14:29

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from ortools.sat.python import cp_model
+
+
 def main():
-    pass
+    num_lecturers = 10
+    num_time_slots = 10
+
+    all_lecturers = range(num_lecturers)
+    all_slots = range(num_time_slots)
+
+    model = cp_model.CpModel()
+    shifts = {}
+    for slot in all_slots:
+
+
 
 if __name__ == "__main__":
     main()
