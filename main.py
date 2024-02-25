637293483d3f24af42b20939b54ce31786bcf729
Hohnik <niklas-hohn@t-online.de>
2024-02-25 17:29:29 +0100

    update dbs and main

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pandas as pd
 from ortools.sat.python import cp_model
 
 # Read the lecturer data from the CSV file using Pandas
-lecturers_df = pd.read_csv("db/lecturers.csv")
+lecturers_df = pd.read_csv("db/lecturers.csv", dtype=str)
 
 # Read the module data from the CSV file using Pandas
-modules_df = pd.read_csv("db/modules.csv")
+modules_df = pd.read_csv("db/modules.csv", dtype=str)
+
 
 # Convert the Pandas DataFrames to Python data structures
 lecturers_data = lecturers_df.to_dict(orient="records")
@@ -19,8 +20,8 @@ model = cp_model.CpModel()
 timetable = {}
 for lecturer in lecturers_data:
     for day in days:
-        for time_slot, bitmask in enumerate(lecturer[day]):
-            if bitmask == "1":
+        for time_slot, bit in enumerate(lecturer[day]):
+            if bit == "1":
                 timetable[(lecturer["lecturer_id"], day, time_slot)] = model.NewBoolVar(
                     f"{lecturer['lecturer_id']}_{day}_{time_slot}"
                 )
@@ -29,32 +30,40 @@ for lecturer in lecturers_data:
 # Lecturers cannot be scheduled for two modules at the same time
 for lecturer in lecturers_data:
     for day in days:
-        for time_slot, bitmask in enumerate(lecturer[day]):
-            if bitmask == '1':
+        for time_slot, bit in enumerate(lecturer[day]):
+            if bit == "1":
                 overlapping_modules = [
-                    timetable[(module['lecturer_id'], day, time_slot)]
+                    timetable[(module["lecturer_id"], day, time_slot)]
                     for module in modules_data
-                    if module['lecturer_id'] == lecturer['lecturer_id']
+                    if module["lecturer_id"] == lecturer["lecturer_id"]
                 ]
                 model.AddImplication(
-                    timetable[(lecturer['lecturer_id'], day, time_slot)],
-                    sum(overlapping_modules) <= 1
+                    timetable[(lecturer["lecturer_id"], day, time_slot)],
+                    len(overlapping_modules) <= 1,
                 )
 
 # Modules that are in the same semester cannot be at the same time
-for module1 in modules_data:
-    for module2 in modules_data:
-        if (
-            module1["semester"] == module2["semester"]
-            and module1["module_id"] != module2["module_id"]
-        ):
-            for day in days:
-                for time_slot in range(10):
-                    model.AddImplication(
-                        timetable[(module1["lecturer_id"], day, time_slot)],
-                        timetable[(module2["lecturer_id"], day, time_slot)]
-                        == 0,
-                    )
+for day in days:
+
+    for module1 in modules_data:
+        for module2 in modules_data:
+            if (
+                module1["semester"] == module2["semester"]
+                and module1["module_id"] != module2["module_id"]
+            ):
+                for lecturer1 in lecturers_data:
+                    for lecturer2 in lecturers_data:
+                        if (lecturer1["lecturer_id"] == module1["lecturer_id"] and 
+                            lecturer2["lecturer_id"] == module2["lecturer_id"] and
+                            lecturer1["lecturer_id"] != lecturer2["lecturer_id"] ):
+                                for time_slot1, bit1 in enumerate(lecturer1[day]):
+                                    for time_slot2, bit2 in enumerate(lecturer2[day]):
+                                        # print(f"{lecturer1['lecturer_id']}:{bit1}:{time_slot1} , {lecturer2['lecturer_id']}:{bit2}:{time_slot2}")
+                                        if bit1 == "1" and bit2 == "1":
+                                            model.Add(
+                                                timetable[(module1["lecturer_id"], day, time_slot1)] +
+                                                timetable[(module2["lecturer_id"], day, time_slot2)] <= 1,
+                                            )
 
 # Solve the model
 solver = cp_model.CpSolver()
@@ -74,9 +83,7 @@ if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
         if lecturer:
             for day in days:
                 for time_slot in range(10):
-                    if solver.Value(
-                        timetable[(module["lecturer_id"], day, time_slot)]
-                    ):
+                    if solver.Value(timetable[(module["lecturer_id"], day, time_slot)]):
                         print(
                             f"Lecturer {lecturer['lecturer_name']} is teaching {module['module_name']} on {day} at time slot {time_slot}"
                         )
