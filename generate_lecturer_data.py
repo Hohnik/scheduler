8decd9c79fecf546466b907948014b46e961ca23
la-s-jsaute <root@JULIENSLP>
2024-02-25 20:19:45 +0100

    create generate_lecturer_data.csv

---
+++ generate_lecturer_data.py
@@ -0,0 +1,49 @@
+# [x] Each lecturer
+# [x] start OR end: 0 OR 00
+# [x] 10 slots per day
+# [ ] 1 day: 0000000000
+# [x] all days: 2-4 random slots = 0
+
+import random
+import pandas as pd
+import pprint
+
+lecturers_df = pd.read_csv("db/lecturers.csv", dtype=str)
+lecturers_data = lecturers_df.to_dict(orient="records")
+days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+lst_bool = [True, False]
+
+lecturers_data_randomized = []
+for lecturer in lecturers_data:
+    for day in days:
+        str_bit = ''
+        start = random.choice(lst_bool)
+        if start:
+            one_zero = random.choice(lst_bool)
+            if one_zero:
+                str_bit += '01'
+            else:
+                str_bit += '00'
+
+        for _ in range(8):
+            chance = random.randint(0, 39)
+            if chance <= 2:
+                str_bit += '0'
+            else:
+                str_bit += '1'
+
+        if not start:
+            one_zero = random.choice(lst_bool)
+            if one_zero:
+                str_bit += '10'
+            else:
+                str_bit += '00'
+                
+        lecturer[day] = str_bit
+    
+    lecturer[random.choice(days)] = '0000000000'
+    
+    lecturers_data_randomized.append(lecturer)
+
+pprint.pprint(lecturers_data)
+pprint.pprint(lecturers_data_randomized)
--- main.py
+++ main.py
@@ -28,41 +28,38 @@ for lecturer in lecturers_data:
 
 # Define the constraints
 # Lecturers cannot be scheduled for two modules at the same time
-# for lecturer in lecturers_data:
-#     for day in days:
-#         for time_slot, bit in enumerate(lecturer[day]):
-#             if bit == "1":
-#                 overlapping_modules = [
-#                     timetable[(module["lecturer_id"], day, time_slot)]
-#                     for module in modules_data
-#                     if module["lecturer_id"] == lecturer["lecturer_id"]
-#                 ]
-#                 model.AddImplication(
-#                     timetable[(lecturer["lecturer_id"], day, time_slot)],
-#                     len(overlapping_modules) <= 1,
-#                 )
+for lecturer in lecturers_data:
+    for day in days:
+        for time_slot, bit in enumerate(lecturer[day]):
+            if bit == "1":
+                overlapping_modules = [
+                    timetable[(module["lecturer_id"], day, time_slot)]
+                    for module in modules_data
+                    if module["lecturer_id"] == lecturer["lecturer_id"]
+                ]
+                model.Add(len(overlapping_modules) <= 1)
 
 # Modules that are in the same semester cannot be at the same time
-# for day in days:
-#    for module1 in modules_data:
-#        for module2 in modules_data:
-#            if (
-#                module1["semester"] == module2["semester"]
-#                and module1["module_id"] != module2["module_id"]
-#            ):
-#                for lecturer1 in lecturers_data:
-#                    for lecturer2 in lecturers_data:
-#                        if (lecturer1["lecturer_id"] == module1["lecturer_id"] and
-#                            lecturer2["lecturer_id"] == module2["lecturer_id"] and
-#                            lecturer1["lecturer_id"] != lecturer2["lecturer_id"] ):
-#                                for time_slot1, bit1 in enumerate(lecturer1[day]):
-#                                    for time_slot2, bit2 in enumerate(lecturer2[day]):
-#                                        # print(f"{lecturer1['lecturer_id']}:{bit1}:{time_slot1} , {lecturer2['lecturer_id']}:{bit2}:{time_slot2}")
-#                                        if bit1 == "1" and bit2 == "1":
-#                                            model.Add(
-#                                                timetable[(module1["lecturer_id"], day, time_slot1)] +
-#                                                timetable[(module2["lecturer_id"], day, time_slot2)] <= 1,
-#                                            )
+for day in days:
+   for module1 in modules_data:
+       for module2 in modules_data:
+           if (
+               module1["semester"] == module2["semester"]
+               and module1["module_id"] != module2["module_id"]
+           ):
+               for lecturer1 in lecturers_data:
+                   for lecturer2 in lecturers_data:
+                       if (lecturer1["lecturer_id"] == module1["lecturer_id"] and
+                           lecturer2["lecturer_id"] == module2["lecturer_id"] and
+                           lecturer1["lecturer_id"] != lecturer2["lecturer_id"] ):
+                               for time_slot1, bit1 in enumerate(lecturer1[day]):
+                                   for time_slot2, bit2 in enumerate(lecturer2[day]):
+                                       # print(f"{lecturer1['lecturer_id']}:{bit1}:{time_slot1} , {lecturer2['lecturer_id']}:{bit2}:{time_slot2}")
+                                       if bit1 == "1" and bit2 == "1":
+                                           model.Add(
+                                               timetable[(module1["lecturer_id"], day, time_slot1)] +
+                                               timetable[(module2["lecturer_id"], day, time_slot2)] <= 1,
+                                           )
 
 # Solve the model
 solver = cp_model.CpSolver()
