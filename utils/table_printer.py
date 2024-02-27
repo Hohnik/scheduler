6e3c6ba402045be5bcccd5989a894a3a698efb95
Hohnik <niklas-hohn@t-online.de>
2024-02-27 16:05:59 +0100

    progresss was made on the table_printer

--- utils/table_printer.py
+++ utils/table_printer.py
@@ -2,22 +2,6 @@
 
 class TablePrinter():
     """
-    {
-        '2': {
-            'monday': {
-                0: [],
-                1: [],
-                2: [['KI240p', 'Auer']],
-                3: [['KI240l', 'Auer']],
-                4: [],
-                5: [['KI250l', 'Eisenreich']],
-                6: [['KI210p', 'Kromer']],
-                7: [],
-                8: [['KI240p', 'Auer']],
-                9: []
-        },
-    }
-
     2      | monday     | tue...
     ____________________________________
     0      | KI240, L01 | ....
@@ -27,34 +11,46 @@ class TablePrinter():
     2      | KI240, L01 | ....
     """
 
-    column_width = 15
+    column_width = 12
+    days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+
+    line_top = "+"+"-"*(column_width//2)+"+"+(("-"*column_width)+"+")*(len(days)-1)+ ("-"*column_width)+"+\n"
+    line = "+"+"-"*(column_width//2)+"+"+(("-"*column_width)+"+")*(len(days)-1)+ ("-"*column_width)+"+\n"
+    line_bot = "+"+"-"*(column_width//2)+"+"+(("-"*column_width)+"+")*(len(days)-1)+ ("-"*column_width)+"+\n"
 
     def __init__(self, solution_object: dict[str, dict[str, dict[int, list]]]) -> None:
         self.solution = solution_object
 
+    def print_tables(self):
+        for table in self.generate_tables():
+            print(table)
 
-    def generate_headers(self):
-        semester = iter(self.solution.keys())
-        result = []
+    def generate_tables(self):
+        return [self._generate_semester(semester) for semester in self.solution.keys()]
+
+    def _generate_semester(self, semester):
+        result = ""
+        result += self._generate_header(semester)
+        result += self._generate_footer()
+
+        return result
+
+
+    def _generate_header(self,semester):
         fields = []
-        while True:
-            try:
-                fields.append(f"{next(semester)}:<{self.column_width}")
-            except:
-                return iter(result)
+        fields.append(f"{semester:^{self.column_width //2}}")
+        fields += list(map(lambda weekday: f"{str.capitalize(weekday):^{self.column_width}}", self.days))
 
-        print()
+        result = ""
+        result += self.line_top
+        result += "|" + "|".join(fields) + "|\n"
+        result += self.line
+        return result
     
-    def get_semester(self):
-        semester = iter(self.solution.keys())
-        print(semester)
-        print("test")
-        while True:
-            try:
-                return 10
-                # yield next(semester)
-            except:
-                pass
+    def _generate_footer(self):
+        result = ""
+        result += self.line_bot
+        return result
 
 
 obj = {
@@ -71,7 +67,11 @@ obj = {
                 8: [['KI240p', 'Auer']],
                 9: []
             },
-        }
+        },
+        "4": {}
     }
-printer = TablePrinter(obj)
-print(printer.get_semester())
+TablePrinter(obj).print_tables()
+# printer = TablePrinter(obj)
+# print(printer._generate_header(2), end="")
+# print(printer._generate_footer(), end="")
+# print(printer._generate_semester(2))
