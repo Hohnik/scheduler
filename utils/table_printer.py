096d8e1b9919ffa4e8ae3baa995b962387c5fa58
Hohnik <niklas-hohn@t-online.de>
2024-03-09 14:45:12 +0100

    update requirements.txt

--- utils/table_printer.py
+++ utils/table_printer.py
@@ -6,16 +6,20 @@ def main():
             "monday": {
                 0: {
                     "lecturer": {"lecturer_id": "001", "lecturer_name": "Osendorfer"},
-                    "module": {"module_id": "lKI210", "module_name": "Programmieren I"},
+                    "module": {"module_id": "lKI210", "module_name": "Programmieren I", "block_sizes_dic": {"1": 1, "2": 2, "3": 3}},
                     "room": {"room_id": "G005"},
+                    "block": "2",
+                    "position": "s",
                 },
             },
         }
     }
 
     print("Here a little test table:")
-    printer = TablePrinter(data)
-    printer.print_semester_tables(end="The print_tables method can be used like a normal print")
+    printer = TablePrinter()
+    printer.set_solution(data)
+    printer.printed_data()
+    printer.print_semester_tables()
 
 class TablePrinter():
     days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
@@ -60,35 +64,37 @@ class TablePrinter():
     def _generate_body(self, semester):
         result = ""
 
+
         for time_slot in range(self._calculate_max_slot() + 1):
             module_fields = []
             lecturer_fields = []
             room_fields = []
-            fields = []
+            joined_fields = []
 
             for day in self.days:
                 try:
-                    data = self.solution[semester][day][time_slot]
+                    slot = self.solution[semester][day][time_slot]
+                    module = f'{slot["module"]["module_id"]}'
+                    lecturer = f'{slot["lecturer"]["lecturer_name"]}'
+                    room = f'{slot["room"]["room_id"]}'
+                    position = f'{slot["position"]}'
+                    block = f'{slot["module"]["block_sizes_dic"][slot["block"]]}'
+                    data_objects = [module, lecturer, room, position, block] # NOTE Print these objects in one line
 
                     if self.print_rows:
-                        module = f'{data["module"]["module_id"]}'
-                        lecturer = f'{data["lecturer"]["lecturer_name"]}'
-                        room = f'{data["room"]["room_id"]}'
-
                         module_fields.append(f'{module:^{self.col_width}}')
                         lecturer_fields.append(f'{lecturer:^{self.col_width}}')
                         room_fields.append(f'{room:^{self.col_width}}')
-                    
                     else:
-                        text = f'{data["position"]}, {data["module"]["block_sizes_dic"][data["block"]]}, {data["module"]["module_id"]}, {data["lecturer"]["lecturer_name"]}'
-                        fields.append(f'{text:<{self.col_width}}')
-
+                        # text = ", ".join(data_objects)
+                        text = f'{position}, {block}, {module}, {lecturer}'
+                        joined_fields.append(f'{text:<{self.col_width}}')
 
                 except KeyError:
                     module_fields.append(f'{"":{self.col_width}}')
                     lecturer_fields.append(f'{"---":^{self.col_width}}')
                     room_fields.append(f'{"":{self.col_width}}')
-                    fields.append(f'{"---":^{self.col_width}}')
+                    joined_fields.append(f'{"":^{self.col_width}}')
                     continue
 
             result += self.line_mid
@@ -103,28 +109,35 @@ class TablePrinter():
                 result += "|".join(room_fields) + "|\n"
             else:
                 result += f'|{time_slot+1:^{self.col_width //2}}|'
-                result += "|".join(fields) + "|\n"
+                result += "|".join(joined_fields) + "|\n"
 
         return result
-    
+
     def _generate_footer(self):
         result = ""
         result += self.line_bot
         return result
 
     def calc_dynamic_col_width(self, solution):
-        spaces = 3
+        spaces = 4
         spaces += 0 if self.print_rows else 1
 
         words = []
         for semester in solution.values():
             for day in semester.values():
                 for slot in day.values():
+                    # pprint.pprint(slot)
                     try: 
-                        words.append(len(slot["lecturer"]["lecturer_name"]) + len(slot["module"]["module_id"]) + len(slot["position"]))
+                        words.append(
+                            len(slot["lecturer"]["lecturer_name"])
+                            + len(slot["module"]["module_id"])
+                            + len(slot["position"])
+                            + len(slot["block"])
+                        )
                     except KeyError:
                         pass
         return max(words) + spaces
+    
 
     def _calculate_max_slot(self):
         result = 0
