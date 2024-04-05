c635fa13a88b3b0de54dd4aee4c34f30064c190c
Hohnik <niklas-hohn@t-online.de>
2024-04-05 14:33:43 +0200

    Add constraint: each slot is assigned to at most one lecturer, module combo

--- test0.py
+++ test0.py
@@ -13,11 +13,14 @@ def main() -> None:
     num_slots = len(data.time_slots)  # 10
     num_days = len(data.days)  # 5
     num_modules = len(data.modules)  # ?
+    num_semester = len(data.semesters)  # ?
 
     all_lecturers = range(num_lecturers)
-    all_slots = range(num_slots)
+    all_timeslots = range(num_slots)
     all_days = range(num_days)
     all_modules = range(num_modules)
+    all_semesters = range(num_semester)
+
     slot_requests = [
         [
             list(map(int, lecturer["monday"])),
@@ -28,6 +31,13 @@ def main() -> None:
         ]
         for lecturer in data.lecturers
     ]
+    # slot_requests = [
+    #     [[one bit for each lecturer], [], [], [], [], [], [], [], [], []], -> Monday
+    #     [[1, 0, 0, 1, 1, 0, 0, 0, 1], [], [], [], [], [], [], [], [], []], -> Tuesday
+    #     [[1, 0, 0, 1, 1, 0, 0, 0, 1], [], [], [], [], [], [], [], [], []], -> Wednesday
+    #     [[1, 0, 0, 1, 1, 0, 0, 0, 1], [], [], [], [], [], [], [], [], []], -> Thursday
+    #     [[1, 0, 0, 1, 1, 0, 0, 0, 1], [], [], [], [], [], [], [], [], []], -> Friday
+    # ]
     preffered_times = [
         [1, 1, 2, 2, 1, 1, 0, 0, 0, 0],
         [1, 2, 3, 3, 2, 2, 1, 1, 0, 0],
@@ -42,48 +52,43 @@ def main() -> None:
     # Creates slot variables.
     # slots[(n, d, s)]: lecturer 'n' works slot 's' on day 'd'.
     bools = {}
-    for d in all_days:
-        for s in all_slots:
-            for m in all_modules:
-                for l in all_lecturers:
-                    bools[(d, s, m, l)] = model.NewBoolVar(
-                        f"module_m{m}_l{l}_d{d}_s{s}"
-                    )
+    for s in all_semesters:
+        for d in all_days:
+            for t in all_timeslots:
+                for m in all_modules:
+                    for l in all_lecturers:
+                        if (
+                            slot_requests[l][d][t]
+                            and data.lecturers[l]["lecturer_id"] in data.modules[m]["lecturer_id"]
+                            and data.semesters[s] == data.modules[m]["semester"]
+                        ):
+                            bools[(s, d, t, m, l)] = model.NewBoolVar(
+                                f"course_s{s}_d{d}_t{t}_m{m}_l{l}"
+                            )
 
     # Each slot is assigned to exactly one lecturer, module combo.
-    for d in all_days:
-        for s in all_slots:
-            model.AddExactlyOne(
-                bools[(d, s, m, l)] for l in all_lecturers for m in all_modules
-            )
+    for s in all_semesters:
+        for d in all_days:
+            for t in all_timeslots:
+                try:
+                    model.AddAtMostOne([
+                        bools[(s, d, t, m, l)] for l in all_lecturers for m in all_modules
+                    ])
+                except KeyError:
+                    pass
 
-    # Try to distribute the slots evenly, so that each lecturer works
-    # min_slots_per_lecturer slots. If this is not possible, because the total
-    # number of slots is not divisible by the number of lecturers, some lecturers will
-    # be assigned one more slot.
-    # min_slots_per_lecturer = (num_slots * num_days) // num_lecturers
-    # if num_slots * num_days % num_lecturers == 0:
-    #     max_slots_per_lecturer = min_slots_per_lecturer
-    # else:
-    #     max_slots_per_lecturer = min_slots_per_lecturer + 1
-    # for n in all_lecturers:
-    #     num_slots_worked = 0
-    #     for d in all_days:
-    #         for s in all_slots:
-    #             num_slots_worked += slots[(n, d, s)]
-    #     model.Add(min_slots_per_lecturer <= num_slots_worked)
-    #     model.Add(num_slots_worked <= max_slots_per_lecturer)
+    preffered_placement = []
+    for m in all_modules:
+        for l in all_lecturers:
+            for d in all_days:
+                for t in all_timeslots:
+                    for s in all_semesters:
+                        try:
+                            preffered_placement.append(preffered_times[d][t] * bools[(s, d, t, m, l)])
+                        except KeyError: 
+                            pass
 
-    model.Maximize(
-        sum(
-            preffered_times[d][s] * bools[(d, s, m, l)]
-            for m in all_modules
-            for l in all_lecturers
-            for d in all_days
-            for s in all_slots
-        )
-    )
-
+    model.Maximize(sum(preffered_placement))
     # Creates the solver and solve.
     # pprint.pprint()
     solver = cp_model.CpSolver()
@@ -91,18 +96,22 @@ def main() -> None:
 
     if status == cp_model.OPTIMAL:
         print("Solution:")
-        for d in all_days:
-            print(data.days[d][:3].capitalize())
-            for m in all_modules:
-                for s in all_slots:
-                    for l in all_lecturers:
-                        if solver.Value(bools[(d, s, m, l)]) == 1:
-                            print(
-                                s,
-                                data.lecturers[l]["lecturer_name"],
-                                data.modules[m]["module_id"],
-                            )
-            print()
+        for s in all_semesters:
+            for d in all_days:
+                print(data.days[d][:3].capitalize())
+                for t in all_timeslots:
+                    for m in all_modules:
+                        for l in all_lecturers:
+                            try:
+                                if solver.Value(bools[(s, d, t, m, l)]) == 1:
+                                    print(
+                                        t,
+                                        data.lecturers[l]["lecturer_name"],
+                                        data.modules[m]["module_id"],
+                                    )
+                            except KeyError:
+                                pass
+                print()
         print(
             f"Number of slot requests met = {solver.ObjectiveValue()}",
         )
